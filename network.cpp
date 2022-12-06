b9bf4dbfa83634033fc5d2105fd32cd3292562a6
batc <89680547+catseonjae@users.noreply.github.com>
2022-12-06 21:26:52 +0900

    Update network.cpp

--- network.cpp
+++ network.cpp
@@ -1,22 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 class matrix{
-  vector<vector<double>> v;
   public:
+    vector<vector<double>> v;
+    int rows, columns;  
     matrix(){
-        
+      rows=columns=1;
     }
-    matrix(int n, int m){
+    matrix(int n, int m) : rows(n), columns(m){
       v=vector<vector<double>>(n,vector<double>(m));
     }
-    matrix(int n, int m, int x){
+    matrix(int n, int m, int x) : rows(n), columns(m){
       v=vector<vector<double>>(n,vector<double>(m,x));
     }
     vector<double> operator [] (int idx){
       return v[idx];
     }
-    matrix operator () (matrix m){
-      
+    matrix operator () (matrix a){
+      matrix ret(rows,a.columns);
+      for(int i=0;i<rows;i++){
+        for(int j=0;j<a.columns;j++){
+          for(int k=0;k<columns;k++){
+            ret[i][j]=ret[i][j]+this[i][k]*a[k][j];
+          }
+        }
+      }
+      return ret;
     }
 }
 
