d0c2f9a8ee2c04c53527ae6b005a4e4d4c4279a9
batc <89680547+catseonjae@users.noreply.github.com>
2022-12-11 01:35:50 +0900

    Update network.cpp
    I separate whole code to main.cpp, network.cpp/h, matrix.h

--- network.cpp
+++ network.cpp
@@ -1,210 +1,113 @@
-#include <bits/stdc++.h>
-using namespace std;
-const double EPSILON=1e-9;
-
-inline double random_number(double s, double e){
-  const double RANDMAX=32767;
-  double r=e-s;
-  double mod = fmod(r,1);
-  
-  double ret = s;
-  for(int k=0;k<(int)r;k++){
-    ret += rand()/RANDMAX;
-    srand((unsigned int)time(NULL));
-  }
-  ret+=rand()*mod/RANDMAX;
-  return ret;  
-}
-inline double random_number(double e){
-  return random_number(0,e);
-}
-class matrix{
-  public:
-    vector<vector<double>> v;
-    int rows, columns;  
-
-    matrix(){
-      rows=columns=1;
-    }
-    matrix(int m, int n) : rows(m), columns(n){
-      v=vector<vector<double>>(m,vector<double>(n));
-    }
-    matrix(int m, int n, double x) : rows(m), columns(n){
-      v=vector<vector<double>>(m,vector<double>(n,x));
-    }
-
-    vector<double> operator [] (int idx){
-      return v[idx];
-    }
-
-    void randomize(double s, double e){
-      
-      for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-          v[i][j]=random_number(s,e);
-        }
-      }
-    }
-    void randomize(double e){
-      randomize(0,e);
-    }
-    matrix operator * (matrix a){
-      matrix ret(rows,a.columns,0.0);
-      for(int i=0;i<rows;i++){
-        for(int j=0;j<a.columns;j++){
-          for(int k=0;k<columns;k++){
-            ret[i][j]=ret[i][j]+(*this)[i][k]*a[k][j];
-          }
-        }
-      }
-      return ret;
-    }
-    matrix operator + (matrix a){
-      matrix ret(rows,columns);
-      for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-          ret[i][j]=(*this)[i][j]+a[i][j];
-        }
-      }
-      return ret;
-    }
-
-    matrix operator - (matrix a){
-      matrix ret(rows,columns);
-      for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-          ret[i][j]=(*this)[i][j]-a[i][j];
-        }
-      }
-      return ret;
-    }
-    vector<vector<double>> to_vector(){
-      return v;
-    }
-	double max_value(){
-		double ret=-DBL_MAX;
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<columns;j++){
-				ret=max(ret,v[i][j]);
-			}
-		}
-		return ret;
-	}
-	matrix foreach(function<double(const double&,const double&)>f,matrix m){
-		if(rows!=m.rows || columns!=m.columns){
-			// error - size does not match
-		}
-		matrix ret(rows,columns);
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<columns;j++){
-				ret[i][j]=f((*this)[i][j],m[i][j]);
-			}
-		}
-		return ret;
-	}
-	matrix pass(function<double(const double&)> f){
-		matrix ret(rows,columns);
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<columns;j++){
-				ret[i][j]=f(v[i][j]);
-			}
-		}
-		return ret;
-	}
-	void print(){
-		for(int i=0;i<rows;i++){
-			for(int j=0;j<columns;j++){
-				cout<<(*this)[i][j]<<" ";
-			}
-			cout<<"\n";
-		}
-	}
-};
-
+#include "network.h"
 namespace activation{
-double sigmoid	(double x){
-		return 1.0/(1+pow(M_E,x));
+	double sigmoid(double x) {
+		return 1.0 / (1 + std::pow(M_E, x));
 	}
-	double relu(double x){
-		return max(0.0, x);
+	double relu(double x) {
+		return std::max(0.0, x);
 	}
 }
-
 namespace lose{
-	double sse(matrix a, matrix t){
-		double ret=0;
-		for(int i=0;i<a.rows;i++){
-			ret+=a[i][0]+t[i][0];
+	double sse(double a, double t){
+		return pow(a-t,2.0)/2.0;
+	}
+	double sse(matrix<double> a, matrix<double> t) {
+		double ret = 0;
+		for (int i = 1; i <= a.get_rows(); i++) {
+			ret += pow(a(i,1)-t(i,1),2.0);
 		}
-		ret/=2;
+		ret /= 2;
 		return ret;
 	}
-	double cee(matrix a, matrix t){
-		for(int i=0;i<a.rows;i++){
-			if(abs(t[i][0])<=EPSILON) continue;
-			return -log(a[i][0]);
+	double cee(double a, double t){
+		return -log(a)*t;
+	}
+	double cee(matrix<double> a, matrix<double> t) {
+		for (int i = 1; i <= a.get_rows(); i++) {
+			if (abs(t(i,1)) <= EPSILON)
+				  continue;
+			return -log(a(i,1));
 		}
 		return 1;
 	}
-	
-	matrix softmax(matrix output){
-		double m=output.max_value()/2.0;
+	matrix<double> softmax(matrix<double> output) {
+		double m = output.max_value() / 2.0;
 		double sum = 0;
-		for(int i=0;i<output.columns;i++){
-			sum+=pow(M_E,output[i][0]-m);
+		for (int i = 1; i <= output.get_columns(); i++) {
+			sum += pow(M_E, output(i,1) - m);
 		}
-		for(int i=0;i<output.columns;i++){
-			output[i][0]=pow(M_E,output[i][0]-m)/sum;
+		for (int i = 1; i < output.get_columns(); i++) {
+			output(i,1) = pow(M_E, output(i,1) - m) / sum;
 		}
 		return output;
 	}
 }
 
-class network{
-  int cur_layer;
-  public:
-    vector<matrix> w;
-    vector<matrix> layers;
-    int depth;
-    network(){
-      depth=1;
-    }
-    network(vector<int> dim){
-      depth=dim.size();
-      for(int i=0;i<dim.size()-1;i++){
-        w.push_back(matrix(dim[i+1],dim[i]));
-        layers.push_back(matrix(dim[i],1));
-      }
-      layers.push_back(matrix(dim.back(),1));
-    }
-
-	  void select_random(){
-  		for(int i=0;i<depth;i++){
-  			w[i].randomize(1,2);
-  		}
-  	}
-    matrix predict(matrix input){
-      layers[0]=input;
-        return matrix(1,1);
-      for(cur_layer=0;cur_layer<depth-1;cur_layer++){
-        layers[cur_layer+1] = (w[cur_layer] * layers[cur_layer]).pass(activation::relu);
-      }
-      return layers.back();
-    }
-    void show_weight(){
-      for(matrix m: w){
-        m.print();
-        cout<<endl<<endl;
-      }
-    }
-};
-void end(){
-	cout<<"everything is ok.";
-  exit(0);
+network::network(std::vector<int> dim, std::function<double(double)> activation_function, std::function<double(matrix<double>,matrix<double>)> lose_function) :activation_function(activation_function), lose_function(lose_function){
+	depth = dim.size();
+	w.push_back(matrix<double>(dim[0+1], dim[0]));
+	layers.push_back(matrix<double>(dim[0]));
+	for (int i = 1; i < dim.size() - 1; i++) {
+		w.push_back(matrix<double>(dim[i + 1], dim[i]));
+		layers.push_back(matrix<double>(dim[i]));
+		gradients.push_back(std::vector<double>(dim[i]));
+	}
+	layers.push_back(matrix<double>(dim.back()));
+	gradients.push_back(std::vector<double>(dim.back()));
 }
-int main() {
-	network nn({3,10});
-	nn.select_random();
-  nn.show_weight();
-	// nn.predict(matrix(3,1).pass(random_number)).print();
+
+void network::back_propagation(matrix<double> &output){
+	for(int i=1;i<=layers.back().get_rows();i++){
+		gradients[layers.size()-1][i-1]=lose_function(output[i],layers.back()[i]+EPSILON)-lose_function(output[i],layers.back()[i]-EPSILON);
+	}
+	for(int i=depth-2;i>=0;i--){
+		for(biginteger r = 1; r<= layers[i].get_rows(); r++){
+			if(i>0) gradients[i-1][r]=0;
+			for(biginteger c = 1; c<= layers[i].get_columns(); c++){
+				double g = layers[i](r,c)*EPSILON*gradients[i][r];
+				layers[i](r,c)-=g*train_rate;
+				if(i>0) gradients[i-1][r]+=g;
+			}	
+		}
+	}
+}
+void network::train(matrix<double> input,matrix<double> output){
+	predict(input);
+	back_propagation(output);
+}
+
+matrix<double> network::predict(matrix<double> input) {
+	if(!layers[0].is_same_size(input)){
+		throw std::runtime_error("in network predict call, matrix sizes are not same\n"+layers[0].info()+input.info());
+	}
+	layers[0] = input;
+	for (cur_layer = 0; cur_layer < depth - 1; cur_layer++) {
+		layers[cur_layer + 1] = (w[cur_layer] * layers[cur_layer]).pass(activation_function);
+	}
+	return layers.back();
+}
+std::string network::info(){
+	std::string ret;
+	ret+="w:\n";
+	ret+="size: "+std::to_string(w.size());
+	ret+="\nsize of matrixs: ";
+	for(matrix<double> i: w){
+		ret+=i.size_info();
+	}
+	//layers: size: {layers.size()} sizeof matrixs: (*),(*),(*),(*) ...
+	ret+="\n\nlayers:\n";
+	ret+="size: "+std::to_string(layers.size());
+	ret+="\nsize of matrixs: ";
+	for(matrix<double> i: layers){
+		ret+=i.size_info();
+	}
+	ret+="\n\nw matrixs:\n";
+	for(matrix<double> i: w){
+		ret+=i.info();
+	}
+	ret+="\nlayers matrixs:\n";
+	for(matrix<double> i:layers){
+		ret+=i.info();
+	}
+	return ret;
 }
